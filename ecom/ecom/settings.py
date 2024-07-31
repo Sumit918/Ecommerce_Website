9895910d078a685a374ac1bea4725c66e93be2ef
Sumit918 <131543199+Sumit918@users.noreply.github.com>
2024-07-31 15:19:53 +0545

    changes

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -94,14 +94,14 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER':'postgres' ,
-        'PASSWORD':'' ,
-        'HOST': 'postgres.railway.internal',
-        'PORT':'5432' ,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'railway',
+        #'USER':'postgres' ,
+        #'PASSWORD':'' ,
+        #'HOST': 'postgres.railway.internal',
+        #'PORT':'5432' ,
    }
 }
 
